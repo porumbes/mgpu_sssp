cafaad513885a00001a1df9cf8d0c7e43c83ff14
Serban Porumbescu <porumbes@users.noreply.github.com>
2021-05-27 23:23:17 -0700

    Add run variant for number of seeds

--- hive-mgpu-run.sh
+++ hive-mgpu-run.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
 
 PARTITION_NAME="dgx2"
+#PARTITION_NAME="batch"
 NODE_NAME="dgx2-000"
-NUM_GPUS=16
+NUM_GPUS=${1:-"16"}
 
 APP_SCRIPT="./hive-vn-test.sh"
+NUM_SEEDS[0]=10
+NUM_SEEDS[1]=100
+NUM_SEEDS[2]=1000
 
 OUTPUT_DIR="vn_eval_mgpu/$PARTITION_NAME"
 mkdir -p $OUTPUT_DIR
 
-for (( i=1; i<=$NUM_GPUS; i++))
+for n_seeds in {0..2}
 do
-    # prepare and run SLURM command
-    #SLURM_CMD="srun --cpus-per-gpu 1 -G $i -p $PARTITION_NAME -w $NODE_NAME"
-    #SLURM_CMD="srun --cpus-per-gpu 1 -G $i -p $PARTITION_NAME "
-
-    SLURM_CMD="srun --cpus-per-gpu 1 -G $i -p $PARTITION_NAME -N 1 "
-    $SLURM_CMD $APP_SCRIPT $OUTPUT_DIR $i &
-
-    #echo "$SLURM_CMD $APP_SCRIPT $OUTPUT_DIR $i"
-    #sleep 1
+   for (( i=1; i<=$NUM_GPUS; i++))
+   do
+       # prepare and run SLURM command
+       #SLURM_CMD="srun --cpus-per-gpu 1 -G $i -p $PARTITION_NAME -w $NODE_NAME"
+       #SLURM_CMD="srun --cpus-per-gpu 1 -G $i -p $PARTITION_NAME "
+   
+       SLURM_CMD="srun --cpus-per-gpu 1 -G $i -p $PARTITION_NAME -N 1 "
+       $SLURM_CMD $APP_SCRIPT $OUTPUT_DIR $i ${NUM_SEEDS[$n_seeds]} &
+   
+       #echo "$SLURM_CMD $APP_SCRIPT $OUTPUT_DIR $i ${NUM_SEEDS[$n_seeds]}"
+       #sleep 1
+   done
 done
--- hive-vn-test.sh
+++ hive-vn-test.sh
@@ -3,14 +3,17 @@
 APP_NAME="main"
 
 BIN_PREFIX="./"
-DATA_PREFIX="./data"
+DATA_PREFIX[0]="./data"
+DATA_PREFIX[1]="/home/u00u7u37rw7AjJoA4e357/data/gunrock/gunrock_dataset/mario-2TB/large"
 
 
-APP_OPTIONS="10"
 
 OUTPUT_DIR=${1:-"eval_mgpu"}
 NUM_GPUS=${2:-"1"}
 JSON_FILE=""
+
+NUM_SEEDS=${3:-"10"}
+APP_OPTIONS=${NUM_SEEDS}
 
 TAG="num-gpus:$NUM_GPUS"
 
@@ -19,23 +22,36 @@ NAME[1]="rmat18"
 NAME[2]="rmat20"
 NAME[3]="rmat22"
 NAME[4]="rmat24"
+NAME[5]="enron"
+NAME[6]="hollywood-2009"
+NAME[7]="indochina-2004"
 
-GRAPH[0]="$DATA_PREFIX/${NAME[0]}.bin"
-GRAPH[1]="$DATA_PREFIX/${NAME[1]}.bin"
-GRAPH[2]="$DATA_PREFIX/${NAME[2]}.bin"
-GRAPH[3]="$DATA_PREFIX/${NAME[3]}.bin"
-GRAPH[4]="$DATA_PREFIX/${NAME[4]}.bin"
+DATA_PATH=${DATA_PREFIX[0]}
+GRAPH[0]="$DATA_PATH/${NAME[0]}.bin"
+GRAPH[1]="$DATA_PATH/${NAME[1]}.bin"
+GRAPH[2]="$DATA_PATH/${NAME[2]}.bin"
+GRAPH[3]="$DATA_PATH/${NAME[3]}.bin"
+GRAPH[4]="$DATA_PATH/${NAME[4]}.bin"
 
-for i in {4..4}
+DATA_PATH=${DATA_PREFIX[1]}
+GRAPH[5]="$DATA_PATH/${NAME[5]}/${NAME[5]}.bin"
+GRAPH[6]="$DATA_PATH/${NAME[6]}/${NAME[6]}.bin"
+GRAPH[7]="$DATA_PATH/${NAME[7]}/${NAME[7]}.bin"
+
+SUB_DIR=${NUM_SEEDS}
+mkdir -p "$OUTPUT_DIR/$SUB_DIR"
+
+for i in {1..7}
 do
    # prepare output json file name with number of gpus for this run
-   JSON_FILE="${APP_NAME}__${NAME[$i]}__GPU${NUM_GPUS}"	
+   JSON_FILE="vn__${NAME[$i]}__GPU${NUM_GPUS}"	
 
    #echo \
    $BIN_PREFIX$APP_NAME \
    ${GRAPH[$i]} \
    $APP_OPTIONS \
-   > "$OUTPUT_DIR/$JSON_FILE.output.txt"
+   "$OUTPUT_DIR/$SUB_DIR/$JSON_FILE.json" \
+   > "$OUTPUT_DIR/$SUB_DIR/$JSON_FILE.output.txt"
    #--tag=$TAG \
    #--jsonfile="$OUTPUT_DIR/$JSON_FILE.json" \
 done
